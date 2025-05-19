373982ff5dfca2f57d98c43a33e6d234146160ad
Gimmy <gm0621@gmail.com>
2025-05-19 23:21:23 +0800

    Step1,2:ThemeData

---
+++ steps/1_setThemeData_custom.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      // theme: ThemeData.dark(),
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF0A0E21), // 主色
+          secondary: Colors.purple,     // 次色
+          brightness: Brightness.dark,  // 若要整體深色風格（可選）
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21), // 這裡設定所有 Scaffold 的背景色
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF0A0E21), // 設定您想要的 AppBar 顏色
+          foregroundColor: Colors.white,     // 設定 AppBar 上的文字/icon 顏色
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+      ),
+    );
+  }
+}
--- steps/2_copyThemeData_dark.dart
+++ steps/2_copyThemeData_dark.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -21,3 +20,27 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+      ),
+    );
+  }
+}
